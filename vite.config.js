b71df8f7324ede9d066f8b55b6ec4021fe0bb594
tsja2001 <2945532754@qq.com>
2023-04-28 13:30:58 +0800

    fix: 修改vite配置: 1/1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import reactRefresh from '@vitejs/plugin-react-refresh';
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
