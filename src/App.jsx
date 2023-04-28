a528460e865c36c0c4279bbba7af0f6f189a8571
tsja2001 <2945532754@qq.com>
2023-04-28 14:23:47 +0800

    feat: 安装antd生态: 1/1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import ChartDemo from './component/chart-demo'
+import ChartTooltip from './component/chart-tooltip'
 import DemoComponent from './component/demo'
+import DemoScatter from './component/demo-scatter'
 
 function App() {
   return (
     <div>
       <DemoComponent></DemoComponent>
+      <ChartDemo></ChartDemo>
+      <ChartTooltip></ChartTooltip>
+      <DemoScatter></DemoScatter>
       <div className="demo">demo</div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '@ant-design/charts/dist/index.css'
 import './style/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
