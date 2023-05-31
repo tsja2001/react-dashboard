55e6d8900b7e7c09475c21fec84b15a066885188
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-05-31 11:48:33 +0800

    feat: 放弃使用antd 样式变量

--- vite.config.js
+++ vite.config.js
@@ -3,39 +3,8 @@ import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
 import cssnano from 'cssnano'
 
-import { createStyleImportPlugin } from 'vite-plugin-style-import'
-import { theme } from 'antd'
-
-import path from 'path'
-
-const { defaultAlgorithm, defaultSeed } = theme
-const mapToken = defaultAlgorithm(defaultSeed)
-
-console.log('mapToken', JSON.stringify(mapToken))
-
 export default defineConfig({
-  plugins: [
-    react(),
-    createStyleImportPlugin({
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: (name) => {
-            // return `/antd/es/${name}/style/index.less`
-            // return path.resolve(
-            //   __dirname,
-            //   `node_modules/antd/es/${name}/style/index.less`
-            // )
-          },
-          resolveComponent: (name) => {
-            // return `/antd/es/${name}/index`
-            // return path.resolve(__dirname, `node_modules/antd/es/${name}/index`)
-          }
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   base: './',
   server: {
     host: '0.0.0.0',
@@ -59,12 +28,6 @@ export default defineConfig({
           preset: 'default'
         })
       ]
-    },
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: mapToken
-      }
     }
   }
 })
